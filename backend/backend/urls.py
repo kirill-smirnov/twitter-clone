3e00e8acab98b8b9f761e4c903d89594f3dcc802
Smirnov Kirill <smirnov.k@yahoo.com>
2018-02-07 02:02:26 +0300

    Created auth with token

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,12 +6,17 @@ from rest_framework import routers
 import posts.views
 import accounts.views
 
+from rest_framework.authtoken import views as rest_framework_views
+
+
 patterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^posts/$', posts.views.PostListView.as_view()),
     url(r'^posts/(?P<pk>\d+)/$', posts.views.PostView.as_view()),
     url(r'^users/$', accounts.views.UserListView.as_view()),
-    url(r'^users/(?P<slug>[A-Za-z0-9]+)/$', accounts.views.UserView.as_view())
+    url(r'^users/(?P<slug>[A-Za-z0-9]+)/$', accounts.views.UserView.as_view()),
+    url(r'^auth/$', rest_framework_views.obtain_auth_token,
+        name='get_auth_token'),
 ]
 
 urlpatterns = [
