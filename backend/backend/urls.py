776c3aaad7fadb160615998a7bfe3e6263618034
Smirnov Kirill <smirnov.k@yahoo.com>
2018-02-07 01:38:32 +0300

    Fix paths

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,10 +6,14 @@ from rest_framework import routers
 import posts.views
 import accounts.views
 
-urlpatterns = [
+patterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^posts/$', posts.views.PostListView.as_view()),
     url(r'^posts/(?P<pk>\d+)/$', posts.views.PostView.as_view()),
-     url(r'^users/$', accounts.views.UserListView.as_view()),
+    url(r'^users/$', accounts.views.UserListView.as_view()),
     url(r'^users/(?P<slug>[A-Za-z0-9]+)/$', accounts.views.UserView.as_view())
+]
+
+urlpatterns = [
+    url(r'^api/', include(patterns)),
 ]
