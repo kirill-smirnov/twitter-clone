1433d06d6736a180960b5600ff3a8c52f74ffffc
Smirnov Kirill <smirnov.k@yahoo.com>
2018-02-21 20:56:52 +0300

    Made header nav frontend routing

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,45 +1,59 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-import isUserAuthenticated from '../utils';
+import Utils from '../utils';
 
 import './header.css';
 
 export default class Header extends Component {
     state = {
-        isMenuShowing: false,
+        isMobileMenuShowing: false,
+        centerMenu: Utils.getMenuRoutes(),
+        activeTab: 0,
     }
 
-    handleClick(e) {
-        this.setState((prev, state) => ({ isMenuShowing: !prev.isMenuShowing }));
+    handleMenuIconClick(e) {
+        this.setState((prev, state) => ({ isMobileMenuShowing: !prev.isMobileMenuShowing }));
+    }
+
+    handleMenuItemClick(i, e) {
+        this.setState({activeTab: i});
     }
 
     render() {
-        const isAuthenticated = isUserAuthenticated();
+        const { isAuthenticated } = this.props;
 
         return (
             <header>
                 <nav className="navbar navbar-dark bg-dark navbar-expand-md">
                     <div className="container">
-                        <a href="/" className="navbar-brand">Logo</a>
-                        {/* data-toggle="collapse" data-target="#header-menu" aria-expanded="false" aria-label="Toggle navigation" */}
-                        <button className="navbar-toggler collapsed" type="button"  onClick={this.handleClick.bind(this)}>
+                        <Link to="/" className="navbar-brand">Logo</Link>
+            
+                        <button className="navbar-toggler collapsed" type="button"  onClick={this.handleMenuIconClick.bind(this)}>
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         
-                        <div className={"navbar-collapse collapse " + (this.state.isMenuShowing ? "show" : "")} id="header-menu">
-                            <ul className="navbar-nav m-auto">
-                                <li className="nav-item active"><a href="/" className="nav-link">Home</a></li>
-                                {isAuthenticated ? 
-                                <li className="nav-item"><a href="/profile/" className="nav-link">Profile</a></li> 
-                                : ""}
-                                <li className="nav-item"><a href="/users/" className="nav-link">Users</a></li>
-                                <li className="nav-item"><a href="/posts/" className="nav-link">Posts</a></li>
-                                {isAuthenticated ? 
-                                <ul class="navbar-nav ml-auto">
-                                    <li className="nav-item"><a href="/logout/" className="nav-link">Logout</a></li>
-                                </ul>
-                                : ""}
+                        <div className={"navbar-collapse collapse " + (this.state.isMobileMenuShowing ? "show" : "")} id="header-menu">
+                            <ul className="navbar-nav navbar-center m-auto">
+                                {
+                                    this.state.centerMenu.map((tab, i) =>  {
+                                        return (
+                                            <li className={
+                                                "nav-item " + (this.state.activeTab === i ? 'active': '')
+                                                + (!(tab.showIfAnonimous && isAuthenticated) ? 'hide' : '')}
+                                                onClick={e => this.handleMenuItemClick(i, e)}
+                                            >
+                                                <Link to={tab.url} className="nav-link">{tab.name}</Link>
+                                            </li>
+                                        )
+                                    })
+                                }
                             </ul>
+                            {isAuthenticated ?
+                            <ul class="navbar-nav navbar-right">
+                                <li className="nav-item"><Link to="/logout/" className="nav-link">Logout</Link></li>
+                            </ul>
+                            : ""}
                         </div>
                     </div>
 
--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,12 +1,41 @@
 import Lockr from 'lockr';
 
-export default () => {
-    let isAuthenticated;
-    let userData = Lockr.get('userdata');
-    if (userData) {
-        isAuthenticated = userData.login !== '';
-    } else {
-        isAuthenticated = false;
+class Utils {
+    static isUserAuthenticated() {
+        let isAuthenticated;
+        let userData = Lockr.get('userdata');
+        if (userData) {
+            isAuthenticated = userData.login !== '';
+        } else {
+            isAuthenticated = false;
+        }
+        return isAuthenticated;
     }
-    return isAuthenticated;
+
+    static getMenuRoutes() {
+        return [
+            {
+                name: 'Home',
+                url: '/home/',
+                showIfAnonimous: true
+            },
+            {
+                name: 'Profile',
+                url: '/profile/',
+                showIfAnonimous: false
+            },
+            {
+                name: 'Users',
+                url: '/users/',
+                showIfAnonimous: true
+            },
+            {
+                name: 'Posts',
+                url: '/posts/',
+                showIfAnonimous: true
+            }
+        ]
+    }
 }
+
+export default Utils;
